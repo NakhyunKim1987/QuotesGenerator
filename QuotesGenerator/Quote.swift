218c1bf2d77ef870b31f229732de85ca2db9721b
nakhyun.kim <kkokkokim15@gmail.com>
2022-06-06 22:33:46 +0900

    frist commit & complete

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by NakHyun Kim on 2022/06/06.
+//
+
+import Foundation
+
+struct Quote {
+    let contents: String
+    let name: String
+}
